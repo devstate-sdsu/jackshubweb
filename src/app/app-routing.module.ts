336d05263d973d9f30834722aa044d21b957571a
Jacob Holst <jacob.holst@daktronics.com>
2019-12-29 13:25:34 -0600

    Renamed event-form and added some comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { SubmitFormComponent } from './pages/submit-form/submit-form.component';
+import { EventFormComponent } from './pages/forms/event-form/event-form.component';
 
+// define routes here
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'submit-event', component: SubmitFormComponent }
+  { path: 'submit-event', component: EventFormComponent }
 ];
 
 @NgModule({
